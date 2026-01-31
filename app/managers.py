596525e6d4664e06988df3e0f4aa376d1aa4c33c
Artur Bertin <arturbertin2006@gmail.com>
2026-01-31 14:09:35 -0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -4,15 +4,16 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self, db_name, table_name) -> None:
+    def __init__(self, db_name: str, table_name: str) -> None:
         self.db_name = db_name
         self.table_name = table_name
         self.con = sqlite3.connect(db_name)
 
-    def create(self, first_name, last_name):
+    def create(self, first_name: str, last_name: str) -> None:
         cur = self.con.cursor()
         cur.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)", (first_name, last_name)
+            f"INSERT INTO {self.table_name}"
+            f"(first_name, last_name) VALUES (?, ?)", (first_name, last_name)
         )
         self.con.commit()
         cur.close()
@@ -31,10 +32,9 @@ class ActorManager:
 
     def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         cur = self.con.cursor()
-        cur.execute(f"UPDATE {self.table_name} SET first_name=?, last_name=? WHERE id=?", (new_first_name, new_last_name, pk,))
-        
+        cur.execute(f"UPDATE {self.table_name} SET first_name=?, last_name=?"
+                    f"WHERE id=?", (new_first_name, new_last_name, pk,))
         cur.close()
-
 
     def delete(self, pk: int) -> None:
         cur = self.con.cursor()
